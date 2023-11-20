22b556fe53cb97958327d679f8ecf348bc9289e4
issach315 <issachemani227@gmial.com>
2023-11-20 15:17:42 +0530

    string

---
+++ Js_String/string.js
@@ -0,0 +1,11 @@
+let myName ="issach";
+
+let remainName=myName.slice(1,myName.length);
+console.log(remainName);
+
+let firstLetter =myName.slice(0,1);
+let firstLetterInName=firstLetter.toUpperCase();
+
+console.log(firstLetterInName+remainName);
+
+console.log(myName.length);
