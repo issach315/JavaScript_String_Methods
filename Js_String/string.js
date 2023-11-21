389bc84c0daf292f2c476761553d4785ebaf33eb
issach315 <issachemani227@gmial.com>
2023-11-21 10:14:48 +0530

    string methods

--- Js_String/string.js
+++ Js_String/string.js
@@ -9,3 +9,10 @@ let firstLetterInName=firstLetter.toUpperCase();
 console.log(firstLetterInName+remainName);
 
 console.log(myName.length);
+
+
+let lastName="Emani";
+
+console.log(lastName.indexOf('m'));// returns the particukar index element
+
+console.log(lastName.includes('E'));// returns the boolean value for Included or not 
