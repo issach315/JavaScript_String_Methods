cfa3ad85e6c428faba0c510f81d6617785f78607
issach315 <issachemani227@gmial.com>
2023-11-21 10:24:49 +0530

    string methods

--- Js_String/string.js
+++ Js_String/string.js
@@ -11,8 +11,23 @@ console.log(firstLetterInName+remainName);
 console.log(myName.length);
 
 
-let lastName="Emani";
+let lastName="Emani ";
 
 console.log(lastName.indexOf('m'));// returns the particukar index element
 
 console.log(lastName.includes('E'));// returns the boolean value for Included or not 
+
+console.log(lastName.concat(myName));
+
+/*
+string.length
+string.indexOf()
+string.includes()
+string.concat()
+string.slice()
+string.spline()
+string.toUpperCase()
+String.toLowerCase()
+string.trim()
+....etc
+*/
